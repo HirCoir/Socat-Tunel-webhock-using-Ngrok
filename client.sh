7bf8e1234a5c60bb4e86a7306d2f7cddac472608
HirCoir <97632765+HirCoir@users.noreply.github.com>
2023-11-02 11:52:00 -0600

    Update client.sh

--- client.sh
+++ client.sh
@@ -2,10 +2,12 @@ pkill ngrok
 pkill ngrok
 pkill ngrok
 ngrok_token="your_ngrok_token_here"
-
 # Definir el puerto para el túnel
 ngrok_port=22
 
+# URL de servidor flask (server.py)
+WEBHOOCK_URL="123.456.789"
+WEBHOOCK_PORT=5000
 # Definir el token secreto de la aplicación Flask
 flask_token="crearurlsecreta"
 
@@ -29,7 +31,7 @@ curl -X POST \
   -H "Content-Type: application/json" \
   -H "Authorization: Token $flask_token" \
   -d '{"url": "'$ngrok_url'"}' \
-  http://129.213.61.110:5000/webhook
+  http://$WEBHOOCK_URL:$WEBHOOCK_PORT=/webhook
 
 # Mostrar información adicional
 echo "URL TCP enviada al servidor webhock"
